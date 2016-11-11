8c6e6a677c1d877adcd7aa9dbafa85b80fc4035e
niyouzhu <niyouzhu@163.com>
2016-11-11 10:36:22 +0800

    added ErrorHandling

---
+++ ErrorHandling/src/ErrorHandling/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ErrorHandling
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [Route("/Error")]
+        public string Index()
+        {
+            return "Customized error page.";
+
+        }
+    }
+}
---
+++ ErrorHandling/src/ErrorHandling/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ErrorHandling
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            //env.EnvironmentName = "Development";
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(new ExceptionHandlerOptions()
+                {
+                    ExceptionHandler = async (context) =>
+                    {
+                        var exception = context.Features.Get<IExceptionHandlerFeature>();
+                        await context.Response.WriteAsync(exception.Error.ToString());
+                    },
+                    ExceptionHandlingPath = "/Error"
+                });
+                //app.UseExceptionHandler("/Error");
+            }
+            //app.UseStatusCodePages();
+            //app.UseStatusCodePagesWithRedirects("~/errors/{0}");
+
+            app.UseMvcWithDefaultRoute();
+            HomePage(app);
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        public void HomePage(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Query.ContainsKey("throw"))
+                {
+                    throw new Exception("Exception triggered!");
+                }
+                var builder = new StringBuilder();
+                builder.AppendLine("<html><body>Hello World!");
+                builder.AppendLine("<ul>");
+                builder.AppendLine("<li><a href=\"/?throw=true\">Throw exception</a></li>");
+                builder.AppendLine("<li><a href=\"/misingpage\">Missing Page</a></li>");
+                builder.AppendLine("</ul>");
+                builder.AppendLine("</body></html>");
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync(builder.ToString());
+                await next.Invoke();
+            });
+        }
+
+    }
+}
